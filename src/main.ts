128ad67decafcfa357e8f95ee8d8705cd7292bf9
Abdessamad Elhamoudi <abdessamad.elhamoudi.external@atos.net>
2026-03-10 03:04:25 +0000

    chore: add vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppComponent } from './app/app.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
 
+
+import { inject } from '@vercel/analytics';
+
+inject();
+
 bootstrapApplication(AppComponent, {
   providers: [
     provideAnimations(),
